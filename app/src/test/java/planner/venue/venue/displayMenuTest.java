08d4da41f6e8e6bd07b4a5ab09c883d8f4de50ea
MatthewKevinBall <matthew.kevin.ball@gmail.com>
2018-05-07 01:08:24 +1200

    restart order button implementation

---
+++ app/src/test/java/planner/venue/venue/displayMenuTest.java
@@ -0,0 +1,21 @@
+package planner.venue.venue;
+
+import android.content.Context;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class displayMenuTest {
+
+
+    @Test
+    public void testReadingMenuString() {
+
+        displayMenu d1 = new displayMenu();
+        assertEquals(d1.totalPrice, 0, 2);
+
+
+
+    }
+}
