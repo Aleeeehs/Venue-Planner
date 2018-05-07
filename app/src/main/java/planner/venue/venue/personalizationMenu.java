2a7af7661c31364ef1e4c39157c95e503cca9ff3
aleeeehs <aleeeehs@icloud.com>
2018-05-07 23:56:51 +1200

    php file for online server testing

--- app/src/main/java/planner/venue/venue/personalizationMenu.java
+++ app/src/main/java/planner/venue/venue/personalizationMenu.java
@@ -1,13 +1,38 @@
 package planner.venue.venue;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Chronometer;
+import android.widget.ImageView;
 
 
 public class personalizationMenu extends AppCompatActivity{
+
+    ImageView profilePhoto;
+    private Chronometer timeSinceClockIn;
+    private long zero = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personalization);
+        profilePhoto = findViewById(R.id.profilePhoto);
+        timeSinceClockIn = (Chronometer)findViewById(R.id.chronometer);
+        startTime();
+    }
+
+    private void startTime()
+    {
+        if(zero == 0)
+        {
+            timeSinceClockIn.setBase(SystemClock.elapsedRealtime());
+            timeSinceClockIn.start();
+            zero +=1;
+        }
+        else
+        {
+
+        }
     }
 }
