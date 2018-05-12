8061e2df9dd86bd5cfbd39b76be311d9a7971dd1
aleeeehs <aleeeehs@icloud.com>
2018-05-12 19:36:24 +1200

    php file for online server testing

--- app/src/main/java/planner/venue/venue/personalizationMenu.java
+++ app/src/main/java/planner/venue/venue/personalizationMenu.java
@@ -3,36 +3,53 @@ package planner.venue.venue;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
 
 
-public class personalizationMenu extends AppCompatActivity{
-
+public class personalizationMenu extends AppCompatActivity {
+    //profile photo
     ImageView profilePhoto;
-    private Chronometer timeSinceClockIn;
-    private boolean running;
+
+    private Button buttonStartTimer, buttonStopTimer;
+    private boolean timerRunning = true;
+    private Chronometer timer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.personalization);
         profilePhoto = findViewById(R.id.profilePhoto);
-        timeSinceClockIn = (Chronometer)findViewById(R.id.chronometer);
-        startTime();
+        setContentView(R.layout.personalization);;
+        buttonStartTimer = findViewById(R.id.button_start_timer);
+        buttonStopTimer =  findViewById(R.id.button_stop_timer);
+        timer = findViewById(R.id.chronometer);
+
+        buttonStartTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(timerRunning)
+                {
+                    timer.setBase(SystemClock.elapsedRealtime());
+                }
+                else
+                {
+
+                }
+                timer.start();
+            }
+
+        });
+
+        buttonStopTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.stop();
+                timer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
     }
 
-    private void startTime()
-    {
-        if(!running)
-        {
-            timeSinceClockIn.setBase(SystemClock.elapsedRealtime());
-            timeSinceClockIn.start();
-            running = true;
-        }
-        else
-        {
-
-        }
-    }
 }
